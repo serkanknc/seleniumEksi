f1a47986e41f09d1fd738c4f4bbbec0dc4af4a8f
Serkan Konukcu <serkankonukcu7@outlook.com>
2024-05-13 19:41:40 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import random
+import time
+
+url = "https://eksisozluk.com/13-mayis-2024-kamuda-tasarruf-tedbirleri--7822008?a=popular&p="
+
+browser = webdriver.Chrome()
+pageCount = 1
+entries = []
+entryCount = 1
+
+while pageCount<=10:
+    randomPage = random.randint(1,50)
+    newUrl = url + str(randomPage)
+    browser.get(newUrl)
+    time.sleep(1)
+
+    elements = browser.find_elements(By.CLASS_NAME,"content")
+    for element in elements:
+        entries.append(element.text)
+    
+    pageCount +=1
+
+with open("entries.txt","w",encoding="UTF-8") as file:
+    for entry in entries:
+        file.write(str(entryCount)+ ".entry\n" + entry + "\n")
+        entryCount+=1
+
+
+browser.close()
+
